c64ea5ccb5035cdfb5ff7b76065cda22cf66bfb0
yuez <zgs225@gmail.com>
2014-01-22 12:39:43 +0800

    refactory rspec test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,32 +3,60 @@ require 'spec_helper'
 describe "静态页面" do
   subject { page }
 
+  shared_examples_for "所有静态页面" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "首页" do
     before { visit root_path }
+    let(:heading) { '欢迎来到易博' }
+    let(:page_title) { '' }
 
-    it { should have_content('易博') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title(' | Home') }
+    it_should_behave_like "所有静态页面"
+    it { should_not have_title(' | 首页') }
   end
 
   describe "帮助页面" do
     before { visit help_path }
+    let(:heading) { '帮助' }
+    let(:page_title) { '帮助' }
 
-    it { should have_content('帮助') }
-    it { should have_title(full_title('帮助')) }
+    it_should_behave_like "所有静态页面"
   end
 
   describe "关于页面" do
     before { visit about_path }
+    let(:heading) { '关于' }
+    let(:page_title) { '关于' }
 
-    it { should have_content('关于') }
-    it { should have_title(full_title('关于')) }
+    it_should_behave_like "所有静态页面"
   end
 
   describe "联系页面" do
     before { visit contact_path }
+    let(:heading) { '联系' }
+    let(:page_title) { '联系' }
 
-    it { should have_content('联系') }
-    it { should have_title(full_title('联系')) }
+    it_should_behave_like "所有静态页面"
+  end
+
+  it "框架中的链接全部正常" do
+    visit root_path
+
+    click_link "首页"
+    expect(page).to have_title(full_title(''))
+
+    click_link "帮助"
+    expect(page).to have_title(full_title('帮助'))
+
+    click_link "注册"
+    expect(page).to have_title(full_title('注册'))
+
+    click_link "关于"
+    expect(page).to have_title(full_title('关于'))
+
+    click_link "联系"
+    expect(page).to have_title(full_title('联系'))
   end
 end
