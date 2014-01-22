28575f046200e1d685fa85e00c9356a57c179298
yuez <zgs225@gmail.com>
2014-01-22 11:30:05 +0800

    change visit method to route path

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,48 +5,48 @@ describe "静态页面" do
 
   describe "首页" do
     it "需要包含标题 ‘易博’" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('易博')
     end
 
     it "需要有正确的首页标题" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{base_title}")
     end
   end
 
   describe "帮助页面" do
     it "需要包含标题 ‘帮助’" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('帮助')
     end
 
     it "需要有正确的帮助页面标题" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | 帮助")
     end
   end
 
   describe "关于页面" do
     it "需要包含标题 ‘关于’" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('关于')
     end
 
     it "需要有正确的关于页面标题" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | 关于")
     end
   end
 
   describe "联系页面" do
     it "需要包含标题‘联系’" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('联系')
     end
 
     it "需要有正确的联系页面标题" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | 联系")
     end
   end
